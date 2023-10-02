f9147ec941531e2d6da6aa1ff078a3e9ab503501
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-10-02 16:50:14 -0300

    atualizar cliente registrando saida e valores

--- App/Controllers/Clientes.php
+++ App/Controllers/Clientes.php
@@ -38,4 +38,61 @@ class Clientes extends Controller{
         }
     }
 
+    public function delete($id){
+        $clienteModel = $this->model("Cliente");
+
+        $clienteModel = $clienteModel->buscarPorId($id);
+
+        if(!$clienteModel){
+            http_response_code(404);
+            echo json_encode(["erro" => "cliente não encontrado"]);
+        }
+
+        $clienteModel = $this->calcularValor($clienteModel);
+
+        $clienteModel->atualizar();
+
+        echo json_encode($clienteModel, JSON_UNESCAPED_UNICODE);
+    }
+
+    private function calcularValor($clienteModel){
+        $dataEntrada = DateTime::createFromFormat("Y-m-d H:i:s", $clienteModel->dataHoraEntrada);
+
+        $dataSaida = new DateTime();
+
+        $intervalo = $dataSaida->diff($dataEntrada);
+        
+        $horas = 0;
+
+        // dia
+        if($intervalo->d > 0){
+            $horas = $horas + $intervalo->d * 24;
+        }
+
+        // hora
+        $horas = $horas + $intervalo->h;
+
+        // minuto
+        // tolerância de 10min
+        if($intervalo->i > 10){
+            $horas = $horas + 1;
+        }
+
+        $precoModel = $this->model("Preco");
+
+        $precoModel = $precoModel->buscarPorId($clienteModel->precoId);
+
+        $clienteModel->valorTotal = $precoModel->primeiraHora;
+
+        $horas--;
+
+        if($horas > 0){
+            $clienteModel->valorTotal = $precoModel->demaisHoras * $horas;
+        }
+
+        $clienteModel->dataHoraSaida = $dataSaida->format("Y-m-d H:i:s");
+
+        return $clienteModel;
+    }
+
 }
--- App/Models/Cliente.php
+++ App/Models/Cliente.php
@@ -51,4 +51,43 @@ class Cliente{
         }
     }
 
+    public function buscarPorId($id){
+        $sql = "SELECT * FROM cliente WHERE id = ? ";
+
+        $stmt = Model::getConn()->prepare($sql);
+        $stmt->bindValue(1, $id);
+
+        if($stmt->execute()){
+            $registro = $stmt->fetch(PDO::FETCH_OBJ);
+
+            if(!$registro){
+                return null;
+            }
+
+            $this->id = $registro->id;
+            $this->nomeCliente = $registro->nome_cliente;
+            $this->placaCarro = $registro->placa_carro;
+            $this->dataHoraEntrada = $registro->data_hora_entrada;
+            $this->dataHoraSaida = $registro->data_hora_saida;
+            $this->valorTotal = $registro->valor_total;
+            $this->precoId = $registro->preco_id;
+
+            return $this;
+        } else {
+            return null;
+        }
+
+    }
+
+    public function atualizar(){
+        $sql = "UPDATE cliente SET data_hora_saida = ?, valor_total = ? WHERE id = ?";
+
+        $stmt = Model::getConn()->prepare($sql);
+        $stmt->bindValue(1, $this->dataHoraSaida);
+        $stmt->bindValue(2, $this->valorTotal);
+        $stmt->bindValue(3, $this->id);
+
+        return $stmt->execute();
+    }
+
 }
